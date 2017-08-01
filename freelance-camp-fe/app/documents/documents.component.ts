c1e0d3f1ac1579468a7c18fca3a0adf312448f27
Dipesh Barakoti <dbarakoti@gmail.com>
2017-08-01 16:05:38 -0600

    Integrated cards into an Angular application

--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -15,21 +15,21 @@ export class DocumentsComponent {
 			description: 'asdfasdfdasdfdsf',
 			file_url: 'http://google.com',
 			updated_at: '07/31/17',
-			image_url: 'http://google.com',
+			image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
 		},
 		{
 			title: "My Second Doc",
 			description: 'asdfasdfdasdfdsf',
 			file_url: 'http://google.com',
 			updated_at: '07/31/17',
-			image_url: 'http://google.com',
+			image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
 		},
 			{
 			title: "My Last Doc",
 			description: 'asdfasdfdasdfdsf',
 			file_url: 'http://google.com',
 			updated_at: '07/31/17',
-			image_url: 'http://google.com',
-		},
+			image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+		}
 	]
 }
