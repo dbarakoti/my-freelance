403b962b359095debba23366f1bf92bffbd9f1d4
Dipesh Barakoti <dbarakoti@gmail.com>
2017-08-01 06:04:14 -0600

    Built a component to create new proposals

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
+import { ProposalNewComponent } from './proposal/proposal-new.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -18,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
 		HomepageComponent
 		DocumentsComponent
 		ProposalListComponent
+		ProposalNewComponent
 
 	],
 	bootstrap: [
