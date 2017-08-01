8f4f3c78d2372a0d5f54fb109d25668e7d22daf3
Dipesh Barakoti <dbarakoti@gmail.com>
2017-08-01 08:54:17 -0600

    Created basic Angular 2 form and implemented the ability to change conteent on the screen in ral time

--- freelance-camp-fe/app/app.module.ts
+++ freelance-camp-fe/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -11,14 +12,15 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
 	imports: [
-	  BrowserModule
-	  AppRoutingModule
+	  BrowserModule,
+	  AppRoutingModule,
+	  FormsModule
 	],
 	declarations: [
 		AppComponent,
-		HomepageComponent
-		DocumentsComponent
-		ProposalListComponent
+		HomepageComponent,
+		DocumentsComponent,
+		ProposalListComponent,
 		ProposalNewComponent
 
 	],
